5765632956dc9b3e4cb1f307aa4abf590016edf8
Ivan Svetunkov <ivan@svetunkov.ru>
2018-05-01 21:13:07 +0000

    version 0.2.1

---
+++ R/laplace.R
@@ -0,0 +1,117 @@
+#' Laplace Distribution
+#'
+#' Density, cumulative distribution, quantile functions and random
+#' generation for the Laplace distribution with the location parameter mu
+#' and Mean Absolute Error (or Mean Absolute Deviation) equal to b.
+#'
+#' When mu=0 and b=1, the Laplace distribution becomes standardized with.
+#' The distribution has the following density function:
+#'
+#' f(x) = 1/(2b) exp(-abs(x-mu) / b)
+#'
+#' @template author
+#' @keywords distribution
+#'
+#' @param q vector of quantiles.
+#' @param p vector of probabilities.
+#' @param n number of observations. Should be a single number.
+#' @param mu vector of location parameters (means).
+#' @param b vector of mean absolute errors.
+#' @param log if \code{TRUE}, then probabilities are returned in
+#' logarithms.
+#'
+#' @return Depending on the function, various things are returned
+#' (usually either vector or scalar):
+#' \itemize{
+#' \item \code{ds} returns the density function value for the
+#' provided parameters.
+#' \item \code{ps} returns the value of the cumulative function
+#' for the provided parameters.
+#' \item \code{qs} returns quantiles of the distribution. Depending
+#' on what was provided in \code{p}, \code{mu} and \code{b}, this
+#' can be either a vector or a matrix, or an array.
+#' \item \code{rs} returns a vector of random variables
+#' generated from the Laplace distribution. Depending on what was
+#' provided in \code{mu} and \code{b}, this can be either a vector
+#' or a matrix or an array.
+#' }
+#'
+#' @examples
+#' x <- dlaplace(c(-1000:1000)/10, 0, 1)
+#' plot(x, type="l")
+#'
+#' x <- plaplace(c(-1000:1000)/10, 0, 1)
+#' plot(x, type="l")
+#'
+#' qlaplace(c(0.025,0.975), 0, c(1,2))
+#'
+#' x <- rlaplace(1000, 0, 1)
+#' hist(x)
+#'
+#' @references \itemize{
+#' \item Wikipedia page on Laplace distribution:
+#' \url{https://en.wikipedia.org/wiki/Laplace_distribution}.
+#' }
+#'
+#' @rdname laplace-distribution
+
+#' @rdname laplace-distribution
+#' @export dlaplace
+#' @aliases dlaplace
+dlaplace <- function(q, mu=0, b=1, log=FALSE){
+    laplaceReturn <- 1/(2*b)*exp(-abs(mu-q)/b);
+    if(log){
+        laplaceReturn <- log(laplaceReturn);
+    }
+    return(laplaceReturn);
+}
+
+#' @rdname laplace-distribution
+#' @export plaplace
+#' @aliases plaplace
+plaplace <- function(q, mu=0, b=1){
+    laplaceReturn <- 0.5 + 0.5*sign(q-mu)*(1-exp(-abs(q-mu)/b));
+    return(laplaceReturn);
+}
+
+#' @rdname laplace-distribution
+#' @export qlaplace
+#' @aliases qlaplace
+qlaplace <- function(p, mu=0, b=1){
+    laplaceReturn <- array(0,c(length(p),length(mu),length(b)),
+                        dimnames=list(paste0("p=",p),paste0("mu=",mu),paste0("b=",b)));
+    laplaceReturn[p==0.5,,] <- 1;
+    laplaceReturn[p==0,,] <- -Inf;
+    laplaceReturn[p==1,,] <- Inf;
+    probsToEstimate <- which(laplaceReturn[,1,1]==0);
+    laplaceReturn[laplaceReturn==1] <- 0;
+    for(k in 1:length(b)){
+        laplaceReturn[probsToEstimate,,k] <- (-b[k] * sign(p[probsToEstimate]-0.5) *
+                                                  log(1-2*abs(p[probsToEstimate]-0.5)));
+    }
+    laplaceReturn <- laplaceReturn + rep(mu,each=length(p));
+    if(any(dim(laplaceReturn)==1)){
+        if(dim(laplaceReturn)[1]==1){
+            laplaceReturn <- laplaceReturn[1,,];
+        }
+        else if(dim(laplaceReturn)[2]==1){
+            laplaceReturn <- laplaceReturn[,1,];
+        }
+        else if(dim(laplaceReturn)[3]==1){
+            laplaceReturn <- laplaceReturn[,,1];
+        }
+
+        if(any(dim(laplaceReturn)==1)){
+            laplaceReturn <- c(laplaceReturn);
+        }
+    }
+    return(laplaceReturn);
+}
+
+#' @rdname laplace-distribution
+#' @export rlaplace
+#' @aliases rlaplace
+rlaplace <- function(n=1, mu=0, b=1){
+    laplaceReturn <- qlaplace(runif(n,0,1),mu,b);
+    return(laplaceReturn);
+}
--- R/svet.R
+++ R/svet.R
@@ -2,7 +2,7 @@
 #'
 #' Density, cumulative distribution, quantile functions and random
 #' generation for the S distribution with the location parameter mu
-#' and half absolute moment equal to ham.
+#' and a scaling parameter b.
 #'
 #' When mu=0 and ham=2, the S distribution becomes standardized with
 #' b=1 (this is because b=ham/2). The distribution has the following
@@ -12,11 +12,14 @@
 #'
 #' The S distribution has fat tails and large excess.
 #'
+#' @template author
+#' @keywords distribution
+#'
 #' @param q vector of quantiles.
 #' @param p vector of probabilities.
 #' @param n number of observations. Should be a single number.
 #' @param mu vector of location parameters (means).
-#' @param ham vector of half absolute moments.
+#' @param b vector of scaling parameter (which equals to ham/2).
 #' @param log if \code{TRUE}, then probabilities are returned in
 #' logarithms.
 #'
@@ -27,31 +30,34 @@
 #' provided parameters.
 #' \item \code{ps} returns the value of the cumulative function
 #' for the provided parameters.
-#' \item \code{qs} returns quantiles of the distribution.
+#' \item \code{qs} returns quantiles of the distribution. Depending
+#' on what was provided in \code{p}, \code{mu} and \code{b}, this
+#' can be either a vector or a matrix, or an array.
 #' \item \code{rs} returns a vector of random variables
-#' generated from the S distribution.
+#' generated from the S distribution. Depending on what was provided
+#' in \code{mu} and \code{b}, this can be either a vector or a matrix
+#' or an array.
 #' }
 #'
 #' @examples
-#' x <- ds(c(-1000:1000)/10, 0, 2)
+#' x <- ds(c(-1000:1000)/10, 0, 1)
 #' plot(x, type="l")
 #'
-#' x <- ps(c(-1000:1000)/10, 0, 2)
+#' x <- ps(c(-1000:1000)/10, 0, 1)
 #' plot(x, type="l")
 #'
-#' qs(c(0.025,0.975), 0, 2)
+#' qs(c(0.025,0.975), 0, 1)
 #'
-#' x <- rs(1000, 0, 2)
+#' x <- rs(1000, 0, 1)
 #' hist(x)
 #'
 #' @rdname s-distribution
 #' @importFrom stats optimize runif
-
+#'
 #' @rdname s-distribution
 #' @export ds
 #' @aliases ds
-ds <- function(q, mu=0, ham=2, log=FALSE){
-    b <- ham / 2;
+ds <- function(q, mu=0, b=1, log=FALSE){
     svetReturn <- 1/(4*b^2)*exp(-sqrt(abs(mu-q))/b);
     if(log){
         svetReturn <- log(svetReturn);
@@ -62,42 +68,61 @@ ds <- function(q, mu=0, ham=2, log=FALSE){
 #' @rdname s-distribution
 #' @export ps
 #' @aliases ps
-ps <- function(q, mu=0, ham=2){
-    b <- ham / 2;
-    svetReturn <- 1/(2*b)*(sqrt(abs(mu-q))+b)*exp(-sqrt(abs(mu-q))/b)*((mu>=q)*1 - (mu<q)*1);
-    svetReturn <- svetReturn + (mu<q)*1;
+ps <- function(q, mu=0, b=1){
+    svetReturn <- 0.5+0.5*sign(q-mu)*(1-1/b*(sqrt(abs(mu-q))+b)*exp(-sqrt(abs(mu-q))/b))
     return(svetReturn);
 }
 
 #' @rdname s-distribution
 #' @export qs
+#' @importFrom lamW lambertWm1
 #' @aliases qs
-qs <- function(p, mu=0, ham=2){
-    b <- ham / 2;
-    cfFunction <- function(q,p,...){
-        return(abs(ps(q,...)-p));
+qs <- function(p, mu=0, b=1){
+    svetReturn <- array(0,c(length(p),length(mu),length(b)),
+                        dimnames=list(paste0("p=",p),paste0("mu=",mu),paste0("b=",b)));
+    svetReturn[p==0.5,,] <- 1;
+    svetReturn[p==0,,] <- -Inf;
+    svetReturn[p==1,,] <- Inf;
+    probsToEstimate <- which(svetReturn[,1,1]==0);
+    svetReturn[svetReturn==1] <- 0;
+    if(length(probsToEstimate)!=0){
+        for(i in 1:length(probsToEstimate)){
+            j <- probsToEstimate[i];
+            for(k in 1:length(b)){
+                if(p[j]<0.5){
+                    svetReturn[j,,k] <- (-b[k]^2*lambertWm1(-2*p[j]/exp(1))^2 -
+                                             2*b[k]^2*lambertWm1(-2*p[j]/exp(1))-b[k]^2);
+                }
+                else{
+                    svetReturn[j,,k] <- (b[k]^2*lambertWm1(2*(p[j]-1)/exp(1))^2 +
+                                             2*b[k]^2*lambertWm1(2*(p[j]-1)/exp(1))+b[k]^2);
+                }
+            }
+        }
+        svetReturn <- svetReturn + rep(mu,each=length(p));
     }
-    svetReturn <- (p==0.5)*0;
-    svetReturn[p==0] <- rep(-Inf,sum(p==0));
-    svetReturn[p==1] <- rep(Inf,sum(p==0));
-    probsToEstimate <- which(svetReturn==0);
-    for(i in 1:length(probsToEstimate)){
-        j <- probsToEstimate[i];
-        solution <- optimize(cfFunction,c(-100:100),mu=0,ham=2,p=p[j]);
-        svetReturn[j] <- solution$minimum;
+    if(any(dim(svetReturn)==1)){
+        if(dim(svetReturn)[1]==1){
+            svetReturn <- svetReturn[1,,];
+        }
+        else if(dim(svetReturn)[2]==1){
+            svetReturn <- svetReturn[,1,];
+        }
+        else if(dim(svetReturn)[3]==1){
+            svetReturn <- svetReturn[,,1];
+        }
+
+        if(any(dim(svetReturn)==1)){
+            svetReturn <- c(svetReturn);
+        }
     }
-    svetReturn <- svetReturn * b^2;
-    svetReturn <- svetReturn + mu;
-    return(svetReturn)
+    return(svetReturn);
 }
 
 #' @rdname s-distribution
 #' @export rs
 #' @aliases rs
-rs <- function(n=1, mu=0, ham=2){
-    svetReturn <- rep(NA,n);
-    for(i in 1:n){
-        svetReturn[i] <- qs(runif(1,0,1),mu,ham);
-    }
+rs <- function(n=1, mu=0, b=1){
+    svetReturn <- qs(runif(n,0,1),mu,b);
     return(svetReturn);
 }
